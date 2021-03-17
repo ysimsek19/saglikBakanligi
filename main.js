1f85bc231f72c85e039e919b57206bb006bf3a8c
yalcinsimsek <ysimsek@karatekin.edu.tr>
2021-03-17 14:21:58 +0300

    ok düzeltme

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 var myChart = echarts.init(document.getElementById('artis'));
-
+var data1=[110.17, 92.45, 85.14, 66.68, 65.57, 56.13, 52.93, 52.60, 45.23, 42.47]
+var data2=[49.78 , 31.65, 20.27, 20.03, 18.72, 14.12, 13.11, 13.07, 9.97, 8.26]
+var up = 'path://M286.031,265l-16.025,3L300,223l29.994,45-16.041-3-13.961,69Z';
+var down = 'path://M216.969,292l16.025-3L203,334l-29.994-45,16.041,3,13.961-69Z'
+//遍历data2里面元素的正负定义一个矢量路径的数组
+var path = [up,up,up,up,up,up,up,up,up,up,]
+var path2 = [down,down,down,down,down,down,down,down,down,down,]
 option = {
     tooltip: {
         trigger: 'axis',
@@ -18,7 +24,14 @@ option = {
         containLabel: true
     },
     yAxis: {
-        type: 'value'
+        type: 'value',
+       nameGap: 300,
+      inverse: false,     //grafiği ters çevirme işlemi
+      boundaryGap: true,
+      splitNumber: 20,
+      
+      
+        
     },
     xAxis: {
         type: 'category',
@@ -26,10 +39,32 @@ option = {
     },
     series: [
         {
+            "name": "",
+            type: 'pictorialBar',
+            symbol: function(data,params){
+                return path[params.dataIndex]
+            },
+            symbolSize: [20, 30],
+            symbolOffset: ['100%', -250],
+            color:'orange',
+            symbolPosition: 'end',
+            label: {
+                
+                formatter:  function(params){
+                    return data1[params.dataIndex]
+                },
+                fontSize: 30,
+                fontWeight: 'bold',
+                color: '#34DCFF'
+            },
+            data: data1
+        },
+        {
             name: 'Yüz binde Vaka Sayısı(27 Şubat-5Mart)',
             type: 'bar',
             color:'blue',
             stack: 'total',
+            
             label: {
                 show: true
             },
@@ -42,7 +77,7 @@ option = {
         {
             name: 'Artis',
             type: 'bar',
-            color:'orange',
+            color:'red',
             stack: 'total',
             label: {
                 show: true
@@ -50,7 +85,7 @@ option = {
             emphasis: {
                 focus: 'series'
             },
-            data: [110.17, 92.45, 85.14, 66.68, 65.57, 56.13, 52.93, 52.60, 45.23, 42.47]
+            data: data1
         }
     ]
 };
@@ -78,13 +113,35 @@ option = {
         containLabel: true
     },
     yAxis: {
-        type: 'value'
+        type: 'value',
+        splitNumber: 20,
     },
     xAxis: {
         type: 'category',
         data: ['SAKARYA', 'ORDU', 'AMASYA', 'RİZE', 'BURDUR', 'GÜMÜŞHANE', 'BİTLİS' , 'BARTIN', 'SİİRT', 'ARTVİN']
     },
-    series: [
+    series: [{
+        "name": "",
+        type: 'pictorialBar',
+        symbol: function(data,params){
+            return path2[params.dataIndex]
+        },
+        symbolSize: [15, 30],
+       // symbolOffset: ['100%', -290],
+        color:'orange',
+        symbolPosition: 'end',
+        label: {
+           
+            formatter:  function(params){
+                return data2[params.dataIndex]
+            },
+            fontSize: 30,
+            fontWeight: 'bold',
+            color: '#34DCFF',
+            
+        },
+        data: data2
+    },
         {
             name: 'Yüz binde Vaka Sayısı(27 Şubat-5Mart)',
             type: 'bar',
@@ -102,7 +159,7 @@ option = {
         {
             name: 'Azalis',
             type: 'bar',
-            color:'orange',
+            color:'green',
             stack: 'total',
             label: {
                 show: true
@@ -110,7 +167,7 @@ option = {
             emphasis: {
                 
             },
-            data: [49.78, 31.65, 20.27, 20.03, 18.72, 14.12, 13.11, 13.07, 9.97, 8.26]
+            data: data2
         }
     ]
 };
